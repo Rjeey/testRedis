2e144d0ba55f1f3c5dd7adb8c77768143fec5c25
Rjeey <licanomega@gmail.com>
2021-02-03 18:25:36 +0300

    first commit

---
+++ src/main/java/com/example/redis/springbootrediscache/config/SwaggerDocumentationConfig.java
@@ -0,0 +1,42 @@
+package com.example.redis.springbootrediscache.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerDocumentationConfig {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Tested Redis")
+                .description("This is Swagger UI environment generated for the Tested version Redis Model:)")
+                .license("")
+                .licenseUrl("http://unlicense.org")
+                .termsOfServiceUrl("")
+                .version("3.0")
+                .contact(new Contact("", "", ""))
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
+                .build()
+                .directModelSubstitute(LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(OffsetDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+}
