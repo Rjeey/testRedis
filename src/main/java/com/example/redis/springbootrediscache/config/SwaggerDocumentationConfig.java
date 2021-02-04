9eb2430557019205393d8fd2ddc21d216299bf7e
Rjeey <licanomega@gmail.com>
2021-02-04 21:48:29 +0300

    config swagger

--- src/main/java/com/example/redis/springbootrediscache/config/SwaggerDocumentationConfig.java
+++ src/main/java/com/example/redis/springbootrediscache/config/SwaggerDocumentationConfig.java
@@ -3,6 +3,7 @@ package com.example.redis.springbootrediscache.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -33,7 +34,8 @@ public class SwaggerDocumentationConfig {
     public Docket customImplementation() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.redis.springbootrediscache.controller"))
+                .paths(PathSelectors.ant("/api/employees/*"))
                 .build()
                 .directModelSubstitute(LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(OffsetDateTime.class, java.util.Date.class)
--- src/main/java/com/example/redis/springbootrediscache/controller/EmployeeController.java
+++ src/main/java/com/example/redis/springbootrediscache/controller/EmployeeController.java
@@ -8,18 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @PostMapping("/employees")
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(@RequestBody Employee employee) {
 
         return employeeRepository.save(employee);
@@ -27,35 +30,35 @@ public class EmployeeController {
 
 
     @GetMapping("/employees")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return ResponseEntity.ok(employeeRepository.findAll());
     }
 
-    @GetMapping("employees/{employeeId}")
+    @GetMapping(value = "/{employeeId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Cacheable(value = "employees",key = "#employeeId")
+    @ResponseStatus(HttpStatus.OK)
     public Employee findEmployeeById(@PathVariable(value = "employeeId") Integer employeeId) {
         System.out.println("Employee fetching from database:: "+employeeId);
         return employeeRepository.findById(employeeId).orElseThrow(
                 () -> new ResouceNotFoundException("Employee not found" + employeeId));
-
     }
 
-
-    @PutMapping("employees/{employeeId}")
+    @PutMapping(value="/{employeeId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @CachePut(value = "employees",key = "#employeeId")
+    @ResponseStatus(HttpStatus.OK)
     public Employee updateEmployee(@PathVariable(value = "employeeId") Integer employeeId,
                                                    @RequestBody Employee employeeDetails) {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new ResouceNotFoundException("Employee not found for this id :: " + employeeId));
         employee.setName(employeeDetails.getName());
-        final Employee updatedEmployee = employeeRepository.save(employee);
-        return updatedEmployee;
-
+        return employeeRepository.save(employee);
     }
 
 
-    @DeleteMapping("employees/{id}")
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @CacheEvict(value = "employees", allEntries = true)
+    @ResponseStatus(HttpStatus.OK)
     public void deleteEmployee(@PathVariable(value = "id") Integer employeeId) {
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 () -> new ResouceNotFoundException("Employee not found" + employeeId));
